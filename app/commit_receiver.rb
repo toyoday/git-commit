5de0976d9470b6c13cb74b17c9a0da1fa16cc910
toyoda <toyoda@localworks.jp>
2018-11-15 20:30:31 +0900

    each.with_index追加

--- app/commit_receiver.rb
+++ app/commit_receiver.rb
@@ -22,16 +22,14 @@ class Commit
     client = Octokit::Client.new login: ENV['LOGIN'], password: ENV['PASSWORD']
     commitlist = client.commits ENV['REPOSITORI']
     compare_list = make_date_diffs.call(Date.today.wday == 1 ? [1, 2, 3] : [1])
-    index = 1
 
-    commitlist.each do |commit|
+    commitlist.each.with_index(1) do |commit, index|
       next unless compare_list.include?(commit[:commit][:author][:date].to_date)
 
       formated_message = commit[:commit][:message].gsub(/\*(.*?)\n/, '')
                                                   .gsub(/(\r\n?|\n)/, '')
       author = commit[:author][:login]
       @message += "#{index}. #{formated_message}(#{author})\n"
-      index += 1
     end
   end
 end
