1929b1a8d3add951f032a8baaf82a9f5592d0e24
toyoda <toyoda@localworks.jp>
2018-11-15 20:29:03 +0900

    initialize追加

--- app/commit_receiver.rb
+++ app/commit_receiver.rb
@@ -5,18 +5,25 @@ require 'date'
 require 'dotenv'
 
 # comment return
-class CommitReceiver
+class Commit
   attr_accessor :message
-  def commit_get
+
+  def initialize
+    # @message = self.class.get_from_github
+    @message = ''
+  end
+
+
+  def get_from_github
     make_date_diffs = lambda { |arr|
       arr.map { |elem| Date.today - elem }
     }
-    @message = ''
     Dotenv.load
     client = Octokit::Client.new login: ENV['LOGIN'], password: ENV['PASSWORD']
     commitlist = client.commits ENV['REPOSITORI']
     compare_list = make_date_diffs.call(Date.today.wday == 1 ? [1, 2, 3] : [1])
     index = 1
+
     commitlist.each do |commit|
       next unless compare_list.include?(commit[:commit][:author][:date].to_date)
 
--- app/slackbot.rb
+++ app/slackbot.rb
@@ -3,12 +3,13 @@ require 'http'
 require 'json'
 require 'byebug'
 
-cr = CommitReceiver.new
-cr.commit_get
+cr = Commit.new
+cr.get_from_github
+
 Dotenv.load
 response = HTTP.post('https://slack.com/api/chat.postMessage', params: {
                        token: ENV['SLACK_API_TOKEN'],
-                       channel: '#dev',
+                       channel: '#test_t',
                        text: cr.message.insert(0, "```\n").insert(-1, '```'),
                        as_user: true
                      })
